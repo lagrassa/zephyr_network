24b0b190bc5fd84fab90691411ec18d5f0718205
Alex LaGrassa <lagrassa@randata>
2015-05-29 20:49:27 -0400

    functional!

--- zephyr_graph.py
+++ zephyr_graph.py
@@ -5,10 +5,19 @@ z = nx.Graph()
 
 
 def alpha_only(c):
-    if str(c).isalpha() and c != ' ':
+    if str(c).isalpha() and c != ' ' and not str(c).isdigit():
         return str(c)
     return ''
 
+def get_writer(last_slash_index,line):
+    writer = ''
+    index = last_slash_index
+    character = line[index]
+    while character != ':':  #checks for timestamp
+        writer+=alpha_only(character)
+        index +=1
+        character = line[index]
+    return writer,index
 f = open('zephyrs.txt', 'r')
 classes_to_subscribers = {}
 
@@ -19,23 +28,15 @@ for line in f:
         if classname not in classes_to_subscribers:
             classes_to_subscribers[classname] = []
         #finds the name of the person writing to the class
-        last_slash_index = line[::-1].index('/');
-        writer = ''
-        index = last_slash_index
-        character = line[index]
-        while character != ':' or index < len(line)-1:
-            writer+=character
-            index +=1
-            try: 
-                character = line[index]
-            except IndexError:
-                print "error", line, index
-        writer = writer[:-2]
+       
+        last_slash_index = line.rfind('/');
+        writer, endindex = get_writer(last_slash_index +1,line) 
+        if writer == "UNAUTH":
+            writer, endindex = get_writer(endindex+1,line)
         if writer not in classes_to_subscribers[classname] and writer !='UNAU':
             classes_to_subscribers[classname].append(writer)
-print classes_to_subscribers
 
-classes_to_subscribers = name_to_friends
+name_to_friends = classes_to_subscribers 
 
 for friend in name_to_friends.keys():
     for other_friend in name_to_friends[friend]:
