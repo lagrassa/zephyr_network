5958154c2630c07dd51b988d35702f4c20a691bc
Alex LaGrassa <lagrassa@randata>
2015-05-29 22:15:46 -0400

    uses barnowl short form to simplify things a lot

--- zephyr_graph.py
+++ zephyr_graph.py
@@ -4,23 +4,23 @@ z = nx.Graph()
 
 
 
-def valid_character_only(c):
-    if (str(c).isalpha() and c != ' ' and not str(c).isdigit()) or c == "_":
-        return str(c)
-    return ''
+#def valid_character_only(c):
+#    if (str(c).isalpha() and c != ' ' and not str(c).isdigit()) or c == "_":
+#        return str(c)
+#    return ''
 
-def get_writer(last_slash_index,line):
-    writer = ''
-    index = last_slash_index
-    try:
-        character = line[index]
-    except IndexError:
-        print line
-    while character != ':' and index < len(line)-1:  #checks for timestamp
-        writer+=valid_character_only(character)
-        index +=1
-        character = line[index]
-    return writer,index
+#def get_writer(last_slash_index,line):
+#    writer = ''
+#    index = last_slash_index
+#    try:
+#        character = line[index]
+#    except IndexError:
+#        print line
+#    while character != ':' and index < len(line)-1:  #checks for timestamp
+#        writer+=valid_character_only(character)
+#        index +=1
+#        character = line[index]
+#    return writer,index
 
 def remove_zsig(line):
     right_parenthesis_index = line.index('(')
@@ -33,22 +33,13 @@ f = open('zephyrs.txt', 'r')
 classes_to_subscribers = {}
 
 for line in f:
-    if line.count('/')>=2 and ':' in line and '(' in line and ')' in line:
-        #detects weird zsig cases
-        if line.count('/')>2:
-            line = remove_zsig(line)
-        classname_slash_index = line.index('/')
-        classname = ''.join([valid_character_only(line[c]) for c in range(classname_slash_index) ])
-        if classname not in classes_to_subscribers:
-            classes_to_subscribers[classname] = []
-        #finds the name of the person writing to the class
-        last_slash_index = line.rfind('/');
-        writer, endindex = get_writer(last_slash_index,line) 
-
-        if writer == "UNAUTH":
-            writer, endindex = get_writer(endindex+1,line)
-        if writer not in classes_to_subscribers[classname] and writer !='UNAU':
-            classes_to_subscribers[classname].append(writer)
+    classname = line[5:19]
+    if classname not in classes_to_subscribers:
+        classes_to_subscribers[classname] = []
+    #finds the name of the person writing to the class
+    writer = line[31:44]
+    if writer not in classes_to_subscribers[classname]:
+        classes_to_subscribers[classname].append(writer)
 
 name_to_friends = classes_to_subscribers 
 for friend in name_to_friends.keys():
